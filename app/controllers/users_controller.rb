416beb95abc7a335ca556396c9ea2549745ab31c
Robin J <robin__j@outlook.com>
2022-01-31 22:12:49 +0530

    implemented signup_feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_param)
+    if @user.save
+      flash[:success] = "You are signed up! Please login."
+      redirect_to login_path
+    else
+      flash[:danger]= @user.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def users_param
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  get 'logout'  => 'sessions#destroy'
+
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#create'
 
   resources :profiles , only: [:update]
   resources :educations , only: [:new]

a1570419398ade9d6d322f206a3afdccc3e45a1e
Robin J <robin__j@outlook.com>
2022-02-01 22:29:31 +0530

    add projects to experiences from edit page

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,7 +1,6 @@
 class ExperiencesController < ApplicationController
   def new
     current_user.profile.experiences.create
-    flash[:success] = "Experience added."
     redirect_to edit_url
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,7 @@
 class ProjectsController < ApplicationController
   def new
+    current_experience = Experience.find(params[:experience_id])
+    current_experience.projects.create
+    redirect_to edit_url
   end
 end
